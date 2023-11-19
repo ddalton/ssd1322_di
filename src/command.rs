5b3bb02a07eb24d659d560704967e39225be2e48
Dilip Dalton <dilipdalton@hotmail.com>
2023-11-18 17:40:00 -0800

    Add init commands

--- src/command.rs
+++ src/command.rs
@@ -6,10 +6,28 @@ use display_interface::{DataFormat, DisplayError, WriteOnlyDataCommand};
 #[derive(Debug)]
 #[allow(dead_code)]
 pub enum Command {
+    Unlock,
     SetColumnAddress(u8, u8),
     SetRowAddress(u8, u8),
+    SetDisplayClock(u8),
+    SetMuxRatio(u8),
+    SetDisplayOffset(u8),
+    SetStartLine(u8),
+    SetRemapFormat(u8, u8),
+    SetGPIO(u8),
+    SetFunctionSelection(u8),
+    SetDisplayEnhancementA(u8, u8),
+    SetContrastCurrent(u8),
+    SetMasterCurrent(u8),
+    SetLinearGrayScaleTable,
+    SetPhaseLength(u8),
+    SetDisplayEnhancementB(u8, u8),
+    SetPrechargeVoltage(u8),
+    SetPrechargePeriod(u8),
+    SetVCOMH(u8),
+    NormalDisplayMode,
+    ExitPartialDisplay,
     WriteRAM,
-    OscFreq(u8),
     DisplayOn,
     DisplayOff,
 }
@@ -23,15 +41,69 @@ impl Command {
         // Some commands are not supported, so the maximum array size needed is only 3 u8 along with the
         // real length
         let (data, len) = match self {
+            // Set command unlock
+            Command::Unlock => ([0xFD, 0x12, 0], 2),
+
             // Set the bounding box
-            Command::SetColumnAddress(start, end) => ([0x15, start, end], 3),
-            Command::SetRowAddress(start, end) => ([0x75, start, end], 3),
+            Command::SetColumnAddress(a, b) => ([0x15, a, b], 3),
+            Command::SetRowAddress(a, b) => ([0x75, a, b], 3),
+
+            // Set the divide and osc freq
+            Command::SetDisplayClock(a) => ([0xB3, a, 0], 2),
+
+            // Set the Multiplex ratio
+            Command::SetMuxRatio(a) => ([0xCA, a, 0], 2),
+
+            // Shift mapping RAM counter
+            Command::SetDisplayOffset(a) => ([0xA2, a, 0], 2),
+
+            // Shift mapping RAM display start line
+            Command::SetStartLine(a) => ([0xA1, a, 0], 2),
+
+            // Set horizontal address increment
+            Command::SetRemapFormat(a, b) => ([0xA0, a, b], 3),
+
+            // GPIO pins
+            Command::SetGPIO(a) => ([0xB5, a, 0], 2),
+
+            // Function selection
+            Command::SetFunctionSelection(a) => ([0xAB, a, 0], 2),
+
+            // Set Display Enhancement A
+            Command::SetDisplayEnhancementA(a, b) => ([0xB4, a, b], 3),
+
+            // Set Contrast current
+            Command::SetContrastCurrent(a) => ([0xC1, a, 0], 2),
+
+            // Set Master current
+            Command::SetMasterCurrent(a) => ([0xC7, a, 0], 2),
+
+            // Set linear gray scale table
+            Command::SetLinearGrayScaleTable => ([0xB9, 0, 0], 1),
+
+            // Set phase length
+            Command::SetPhaseLength(a) => ([0xB1, a, 0], 2),
+
+            // Set Display Enhancement B
+            Command::SetDisplayEnhancementB(a, b) => ([0xD1, a, b], 3),
+
+            // Set pre-charge voltage
+            Command::SetPrechargeVoltage(a) => ([0xBB, a, 0], 2),
+
+            // Set pre-charge period
+            Command::SetPrechargePeriod(a) => ([0xB6, a, 0], 2),
+
+            // Set common pins voltage level
+            Command::SetVCOMH(a) => ([0xBE, a, 0], 2),
+
+            // Set normal display mode
+            Command::NormalDisplayMode => ([0xA6, 0, 0], 1),
+
+            // Exit partial display
+            Command::ExitPartialDisplay => ([0xA9, 0, 0], 1),
 
             // Write the data following this command
             Command::WriteRAM => ([0x5C, 0, 0], 1),
-
-            // Increase display frame rate
-            Command::OscFreq(fosc) => ([0xB3, ((0xF & fosc) << 4), 0], 2),
 
             // Sleep mode off
             Command::DisplayOn => ([0xAF, 0, 0], 1),
--- src/display.rs
+++ src/display.rs
@@ -46,15 +46,34 @@ impl<DI: WriteOnlyDataCommand> Ssd1322<DI> {
         delay.delay_ms(10);
 
         rst.set_high().map_err(|_| DisplayError::BusWriteError)?;
-        delay.delay_ms(10);
+        delay.delay_ms(200);
 
         Ok(())
     }
 
     /// Initializes the display.
     pub fn init(&mut self) -> Result<(), DisplayError> {
+        self.send_command(Command::Unlock)?;
         self.send_command(Command::DisplayOff)?;
-        self.send_command(Command::OscFreq(0xF))?;
+        self.send_command(Command::SetColumnAddress(0x1C, 0x5B))?;
+        self.send_command(Command::SetRowAddress(0x00, 0x3F))?;
+        self.send_command(Command::SetDisplayClock(0x91))?;
+        self.send_command(Command::SetMuxRatio(0x3F))?;
+        self.send_command(Command::SetDisplayOffset(0x00))?;
+        self.send_command(Command::SetStartLine(0x00))?;
+        self.send_command(Command::SetRemapFormat(0x14, 0x11))?;
+        self.send_command(Command::SetGPIO(0x00))?;
+        self.send_command(Command::SetFunctionSelection(0x01))?;
+        self.send_command(Command::SetDisplayEnhancementA(0xA0, 0xFD))?;
+        self.send_command(Command::SetContrastCurrent(0x9F))?;
+        self.send_command(Command::SetMasterCurrent(0x0F))?;
+        self.send_command(Command::SetLinearGrayScaleTable)?;
+        self.send_command(Command::SetPhaseLength(0xE2))?;
+        self.send_command(Command::SetDisplayEnhancementB(0xA2, 0x20))?;
+        self.send_command(Command::SetPrechargeVoltage(0x1F))?;
+        self.send_command(Command::SetPrechargePeriod(0x08))?;
+        self.send_command(Command::SetVCOMH(0x07))?;
+        self.send_command(Command::NormalDisplayMode)?;
         self.send_command(Command::DisplayOn)?;
 
         Ok(())
@@ -67,6 +86,14 @@ impl<DI: WriteOnlyDataCommand> Ssd1322<DI> {
 
     /// Flushes the display, and makes the output visible on the screen.
     pub fn flush(&mut self) -> Result<(), DisplayError> {
+        self.send_command(Command::SetColumnAddress(
+            0,
+            (DISPLAY_WIDTH / 4 - 1).try_into().unwrap(),
+        ))?;
+        self.send_command(Command::SetRowAddress(
+            0,
+            (DISPLAY_HEIGHT - 1).try_into().unwrap(),
+        ))?;
         self.send_command(Command::WriteRAM)?;
         self.display.send_data(U8(&self.buffer))
     }
@@ -84,15 +111,13 @@ impl<DI> DrawTarget for Ssd1322<DI> {
             // Check if the pixel coordinates are out of bounds (negative or greater than
             // (255,63)). `DrawTarget` implementation are required to discard any out of bounds
             // pixels without returning an error or causing a panic.
-            if let (x @ 0..=255, y @ 0..=63) = (coord.x as u8, coord.y as u8) {
+            if let (x @ 0..=255, y @ 0..=63) = (coord.x as usize, coord.y as usize) {
                 // Calculate the index in the framebuffer.
-                let index: u8 = x / 2 + y * 128;
+                let index = x / 2 + y * DISPLAY_WIDTH / 2;
                 if x % 2 == 0 {
-                    self.buffer[index as usize] =
-                        update_upper_half(self.buffer[index as usize], color.luma());
+                    self.buffer[index] = update_upper_nibble(self.buffer[index], color.luma());
                 } else {
-                    self.buffer[index as usize] =
-                        update_lower_half(self.buffer[index as usize], color.luma());
+                    self.buffer[index] = update_lower_nibble(self.buffer[index], color.luma());
                 }
             }
         }
@@ -118,11 +143,11 @@ impl<DI> OriginDimensions for Ssd1322<DI> {
 }
 
 #[inline]
-fn update_upper_half(input: u8, color: u8) -> u8 {
+fn update_upper_nibble(input: u8, color: u8) -> u8 {
     color << 4 | (input & 0x0F)
 }
 
 #[inline]
-fn update_lower_half(input: u8, color: u8) -> u8 {
+fn update_lower_nibble(input: u8, color: u8) -> u8 {
     color & 0x0f | (input & 0xF0)
 }
